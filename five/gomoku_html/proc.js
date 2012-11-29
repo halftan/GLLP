4701cb57bf8fc0b798a583b511ca5a33590c438a
HalFtaN <half.a.tan@gmail.com>
2012-11-30 01:29:35 +0800

    json... Signed-off-by: HalFtaN <half.a.tan@gmail.com>

--- five/gomoku_html/proc.js
+++ five/gomoku_html/proc.js
@@ -18,7 +18,29 @@ function nameChanged(v, index){
 }
 
 function history_load(){
-		
+    clear_board(true);
+	if(typeof(Storage) !== "undefined"){
+        history_vector = JSON.parse(localStorage.getItem("History"));
+        _whosTurn = BLACK;
+        _isEnabled = false;
+        history_current_step = 0;
+        alert("History Loaded.");
+    }
+}
+
+function clear_board(isreplay){
+    var t;
+    while(t = history_vector.shift()){
+        bdmap[parseInt(t[0])] = BLANK;
+        t = document.getElementById(t[0]);
+        t.innerHTML = "";
+    }
+    if(!isreplay){
+        _whosTurn = BLACK;
+        _isEnabled = true;
+    }
+    t = document.getElementById("notice");
+    t.innerHTML = "";
 }
 
 function history_save(){
@@ -26,20 +48,46 @@ function history_save(){
 		alert("Not ended!");
 		return;
 	}
-	localStorage.setItem("History", history_vector);
+    localStorage.setItem("History", JSON.stringify(history_vector));
 }
 
 
 function history_back(){
-	if(history_current_step == undefined || history_current_step == 0)
+	if(history_current_step <= 1)
 		return;
-	
+    if(history_current_step == undefined){
+        // one turn backward
+      var l_one = history_vector.pop()[0];
+      var l_two = history_vector.pop()[0];
+      var loc_one = document.getElementById(l_one);
+      var loc_two = document.getElementById(l_two);
+      loc_one.innerHTML = "";
+      loc_two.innerHTML = "";
+      bdmap[parseInt(l_one)] = BLANK;
+      bdmap[parseInt(l_two)] = BLANK;
+    }
+    else{
+        var loc_bac = document.getElementById(history_vector.pop()[0]);
+        loc_bac.innerHTML = "";
+        _whosTurn = 3 - _whosTurn;
+    }	
 }
 
 function history_forward(){
-	if(history_current_step == undefined)
-		return;
-	
+	if(history_current_step == undefined){
+        alert("History not loaded!");
+        return;
+    }
+    var his_t = history_vector.shift();
+    if(!his_t){
+        alert("Replay ended!");
+        return;
+    }
+    setPiece_by_his(his_t[0]);
+    if(_whosTurn != 3 - his_t[1]){
+        alert("Error occured in history_set_piece");
+        _whosTurn = 3 - _whosTurn;
+    }
 }
 
 function hasFive(loc, patt){
@@ -92,6 +140,17 @@ function hasFive(loc, patt){
     return false;
 }
 
+function setPiece_by_his(id){
+    var loc = document.getElementById(id);
+    var pic = document.createElement("img");
+    if(_whosTurn == BLACK)
+        pic.src = "./res/blackp.png";
+    else
+        pic.src = "./res/whitep.png";
+    loc.appendChild(pic);
+    _whosTurn = 3 - _whosTurn;
+}
+
 function setPiece(id){
     if(_isEnabled == false || bdmap[parseInt(id)] != BLANK)
         return;
@@ -103,16 +162,16 @@ function setPiece(id){
         pic.src = "./res/whitep.png";
     loc.appendChild(pic);
     bdmap[parseInt(id)] = _whosTurn;
+    history_vector.push([id,_whosTurn]);
     if(hasFive(id, _whosTurn)){
         _isEnabled = false;
         document.getElementById("notice").
             innerHTML = "Winner is :";
-       document.getElementById("notice").innerHTML += _whosTurn == BLACK ? 
-            document.getElementById("p1_name").innerHTML : 
-            document.getElementById("p2_name").innerHTML;
+        document.getElementById("notice").innerHTML += _whosTurn == BLACK ? 
+        document.getElementById("p1_name").innerHTML : 
+        document.getElementById("p2_name").innerHTML;
             return;
     }
-	history_vector.push([id,_whosTurn]);
     _whosTurn = 3 - _whosTurn;
 }
 
